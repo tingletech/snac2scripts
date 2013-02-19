cf1258545a19c53cf2e07e91ab3565f8626d0a92
Brian Tingle <brian.tingle@ucop.edu>
2013-02-18 19:57:28 -0500

    add eac entity type as cleo title

--- load-cleo.py
+++ load-cleo.py
@@ -35,15 +35,26 @@ def main(argv=None):
             # limit to XML files by file extension
             if os.path.splitext(file)[1] == '.xml':
                 filename = os.path.join(path,file)
-                # this will return False if file has problems
-                read_name  = read_file(filename)
-                if read_name:
-                    counter = counter + 1
-                    name = u" ".join(read_name)
-                    # side effect on output
-                    add_element(output, name, counter, 1)
+                # http://stackoverflow.com/a/3448733/1763984
+                try:
+                    read_name, read_title = read_file(filename)
+                except MyReadError:
+                    break
+                # not sure if the identifier has to be a number
+                counter = counter + 1
+                # convert from array/nodelist to unicode string
+                name = u" ".join(read_name)
+                title = u" ".join(read_title)
+                # side effect on output xml
+                add_element(output, name, title, counter, 1)
     # write out xml file
     doc.write('/home/btingle/python/test.xml', encoding='utf-8', pretty_print=True)
+
+class MyReadError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
     
 def read_file(file):
     """read the identity strings from the XML file"""
@@ -52,29 +63,33 @@ def read_file(file):
         tree = etree.parse(file)
     # http://stackoverflow.com/a/730778/1763984
     except Exception: 
-        return False
+        raise MyReadError("could not open")
     # could be XML, but not EAC-CPF
     if tree.xpath("/eac:eac-cpf", namespaces={'eac': 'urn:isbn:1-931666-33-4'}) == None:
-        return False
+        raise MyReadError("not EAC")
     # grab all name parts, sloppy to return array/node set?
-    return tree.xpath("/eac:eac-cpf/eac:cpfDescription/eac:identity/eac:nameEntry/eac:part/text()",
-              namespaces={'eac': 'urn:isbn:1-931666-33-4'})
+    return (tree.xpath("/eac:eac-cpf/eac:cpfDescription/eac:identity/eac:nameEntry/eac:part/text()",
+              namespaces={'eac': 'urn:isbn:1-931666-33-4'}), 
+            tree.xpath("/eac:eac-cpf/eac:cpfDescription/eac:identity/eac:entityType/text()",
+              namespaces={'eac': 'urn:isbn:1-931666-33-4'}), 
+           )
 
-def add_element(tree, string, id, score):
+def add_element(tree, name, title, id, score):
+     # create some XML
      element = etree.SubElement(tree, "element")
      id_e = etree.SubElement(element, "id")
      id_e.text = unicode(id)
-     name = etree.SubElement(element, "name")
-     name.text = string
-     title = etree.SubElement(element, "title")
-     title.text = u'person'
+     name_e = etree.SubElement(element, "name")
+     name_e.text = name
+     title_e = etree.SubElement(element, "title")
+     title_e.text = title
      score_e = etree.SubElement(element, "score")
      score_e.text = unicode(score)
      # compile a regex to match all non-word characters (unicode aware)
      cleaner = re.compile('\W+', re.UNICODE)
      # MARC is decomposed, normalize to compatiable/combined form, or the cleaner regex will match combining characters
-     string = unicodedata.normalize('NFKC', string)
-     clean_string = cleaner.sub(' ', string)
+     name = unicodedata.normalize('NFKC', name)
+     clean_string = cleaner.sub(' ', name)
      for term in clean_string.split():
         term_e = etree.SubElement(element, "term")
         term_e.text = term.lower()
